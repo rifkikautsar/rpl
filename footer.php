4c79db5ef591125bec5b354e29c568a475937607
biner009 <rifkikautsar1@gmail.com>
2021-07-13 18:28:44 +0700

    fix footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <!-- Footer-->
-<footer class="py-5 bg-inf">
+<footer class="py-5 bg-info">
     <div class="container">
         <p class="m-0 text-center text-white">&copy;iCendol. All Right Reserved</p>
     </div>

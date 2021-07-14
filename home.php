d84d2897645cd3229ca2d7d111239d744e42ae2a
biner009 <rifkikautsar1@gmail.com>
2021-07-14 14:01:43 +0700

    mengedit home.php

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <section class="jumbotron jumbotron-fluid text-center bg-custom home">
     <div class="container">
         <h1 class="display-4 p-3 pt-5">Selamat Datang di iCendol</h1>
-        <p class="lead p-3">Restoran Cendol Nomor 1 di Indonesia</p>
+        <p class="lead p-3">Restoran Cendol Nomor 1 di Dunia</p>
     </div>
     <!--Footer-->
 
